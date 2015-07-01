2e3aad90fde3f362fab98864bb2d8d0d210f3f5e
marek <marek@localhost.localdomain>
2015-07-01 20:43:08 +0200

    initial commit

---
+++ src/clj/todo_app/server.clj
@@ -0,0 +1,9 @@
+(ns todo-app.server
+  (:require [todo-app.handler :refer [app]]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
+
+ (defn -main [& args]
+   (let [port (Integer/parseInt (or (env :port) "3000"))]
+     (run-jetty app {:port port :join? false})))
